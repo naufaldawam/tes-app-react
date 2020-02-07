2ebf6296cbdae75b5d47a856689c3b958acdbf52
uciha1ndra <indranofiyan@gmail.com>
2020-02-07 14:56:11 +0700

    ansdksnfkjnksajnknaksinl

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React app js fun 
         </a>
       </header>
     </div>
