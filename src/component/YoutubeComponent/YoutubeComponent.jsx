1e52a09095de7cbe79a4acfc53af81f0e7a0383d
fall-dawam <nauma625@gmail.com>
2020-02-10 16:45:14 +0700

    Fixxing css

--- src/component/YoutubeComponent/YoutubeComponent.jsx
+++ src/component/YoutubeComponent/YoutubeComponent.jsx
@@ -7,6 +7,7 @@ const YoutubeComponent = () => {
             <div className="img-thumbnail">
                 <img src = "" alt = ""/>
             </div>
+            <p className="time">7.20</p>
             <p className="title">Title here</p>
             <p className="desc">Desc here</p>
         </div>
